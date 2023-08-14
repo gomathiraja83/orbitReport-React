bbd66848e1926ce27b5c2ecbfab40bcf2049a2fa
gomathiraja83 <gomathimurugan@gmail.com>
2023-08-14 18:14:33 -0500

    Assignment 6 coding done

---
+++ src/components/Banner.js
@@ -0,0 +1,11 @@
+export default function Banner()
+{
+    return (
+    <div>
+        <header><b>Orbit Report</b></header>
+        <p>Click on the buttons to see the satellites in that orbit type</p>
+        </div>);
+          
+}
+
+//export default Banner;
